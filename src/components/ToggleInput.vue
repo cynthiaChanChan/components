5d3e60e74a6e94096abc5fe66610d86d6acfbe5c
cynthiaChanChan <mession01@gmail.com>
2019-07-04 19:13:22 +0800

    1. Building Controlled Components

---
+++ src/components/ToggleInput.vue
@@ -0,0 +1,18 @@
+<template>
+	<span class="toggle flex-no-shrink" role="checkbox" tabindex="0" 
+		:aria-checked="value.toString()" 
+		@click="toggle" 
+		@keydow.space.prevent="toggle">		
+	</span>
+</template>
+
+<script>
+	export default {
+		props: ['value'],
+		methods: {
+			toggle() {
+				this.$emit('input', !this.value);
+			}
+		}
+	}
+</script>
